8924b35113bd001f361cd204f53d9a8e136a3e35
Akoaya Nsengue Benz Brown <116504122+Benz-Ak@users.noreply.github.com>
2025-07-03 22:02:58 +0100

    changements des composants FeatureCard en lien

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,12 +4,17 @@ import {
   FaExclamationTriangle,
 } from "react-icons/fa";
 import logo from "../src/assets/images/logoPAD.jpg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#0f172a] via-[#1e293b] to-[#0f172a] text-gray-100 flex flex-col items-center justify-center p-8">
       <header className="mb-16 text-center flex flex-col items-center max-w-3xl">
-          <img src={logo} alt="" className="rounded-full mb-4 w-[50%] aspect-square"/>
+        <img
+          src={logo}
+          alt=""
+          className="rounded-full mb-4 w-[50%] aspect-square"
+        />
         <h1 className="text-3xl font-extrabold mb-4 drop-shadow-md">
           Bienvenue sur MeteoMarinePAD
         </h1>
@@ -23,16 +28,19 @@ export default function Home() {
 
       <section className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-5xl w-full">
         <FeatureCard
+          href={"/forecast"}
           icon={<FaCloudSun size={40} className="text-yellow-400" />}
           title="Prévisions Précises"
           description="Consultez les prévisions météorologiques pour les jours à venir avec des données fiables  et régulièrement mises à jour."
         />
         <FeatureCard
+          href={"/map"}
           icon={<FaMapMarkedAlt size={40} className="text-cyan-400" />}
           title="Cartes Interactives"
           description="Visualisez les stations météo du PAD (SM1,SM2,SM3,SM4) sur une carte interactive pour mieux comprendre les conditions locales."
         />
         <FeatureCard
+          href={"/alerts"}
           icon={<FaExclamationTriangle size={40} className="text-red-500" />}
           title="Alertes & Conseils"
           description="Recevez des alertes sur les conditions météorologiques dangereuses accompagnées de recommandations d’experts."
@@ -46,12 +54,12 @@ export default function Home() {
   );
 }
 
-function FeatureCard({ icon, title, description }) {
+function FeatureCard({ icon, title, description, href }) {
   return (
-    <div className="bg-white/10 backdrop-blur-md rounded-xl p-6 shadow-lg text-center flex flex-col items-center gap-4 hover:scale-[1.03] transition-transform duration-300 cursor-default">
+    <Link to={href} className="bg-white/10 backdrop-blur-md rounded-xl p-6 shadow-lg text-center flex flex-col items-center gap-4 hover:scale-[1.03] transition-transform duration-300 cursor-default">
       <div>{icon}</div>
       <h3 className="text-xl font-semibold">{title}</h3>
       <p className="text-gray-300">{description}</p>
-    </div>
+    </Link>
   );
 }
